21f9872ed92adc7ba04aefb39a99b6068969241f
haribulagakula <haribulagakula@gmail.com>
2024-10-07 15:28:01 -0400

    Name changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                         variants={container(0)}
                         initial="hidden"
                         animate="visible" 
-                        className="pb-8 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Bulagakula Srihari</motion.h1>
+                        className="pb-8 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Srihari Bulagakula</motion.h1>
                     <motion.span
                         variants={container(0.5)}
                         initial="hidden"

956ee170b99650fdc4819793072196c406a295dc
haribulagakula <haribulagakula@gmail.com>
2024-10-07 21:36:18 -0400

    added new photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/profile.jpg"
+//import profilePic from "../assets/profile.jpg"
+import profilePic from "../assets/photo.JPG"
 import { motion } from "framer-motion";
 
 const container = (delay) => ({

9bd6ad9d09dd4370f01c8a497baee74ed39de987
haribulagakula <haribulagakula@gmail.com>
2024-10-07 21:48:16 -0400

    adding new file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/photo.JPG"
+import profilePic from "../assets/photo.jpg"
 import { motion } from "framer-motion";
 
 const container = (delay) => ({

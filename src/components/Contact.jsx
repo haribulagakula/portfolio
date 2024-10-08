ce7bfe3f011c40d933e64f0711688390624048b2
haribulagakula <haribulagakula@gmail.com>
2024-10-07 23:59:47 -0400

    test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,11 +15,11 @@ const Contact = () => {
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 1 }}
                 className="my-4">{CONTACT.address}</motion.p>
-                <motion.b 
+                <motion.p 
                 whileInView={{ opacity: 1, x: 0 }}
-                initial={{ opacity: 0, x: -100 }}
+                initial={{ opacity: 0, x: 0 }}
                 transition={{ duration: 1 }}
-                className="my-4">{CONTACT.phone}</motion.b>
+                className="my-4">{CONTACT.phone}</motion.p>
             <motion.a 
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: 100 }}

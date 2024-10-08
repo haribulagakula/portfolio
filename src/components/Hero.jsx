b22af0cb1e8b5533b1f7bdaf92cd08f0e88dfeda
haribulagakula <haribulagakula@gmail.com>
2024-10-07 21:50:40 -0400

    image adjustment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             <div className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex justify-center">
                     <motion.img
-                        initial={{x: 90, opacity: 0}}
+                        initial={{x: 110, opacity: 0}}
                         animate={{x: 0, opacity: 1}}
                         transition={{duration: 1, delay: 1.2}}
                         className="rounded-lg max-w-xl" 

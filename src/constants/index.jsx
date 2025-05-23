4e03d10561ddac09aac1ac20e1e48dad6b95d193
haribulagakula <haribulagakula@gmail.com>
2025-05-23 13:27:32 -0400

    Update index.jsx
    testing

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -7,7 +7,45 @@ has equipped me with the skills and knowledge to lead and deliver complex projec
 export const ABOUT_TEXT =`I am proficient in developing plug-ins in IBM Content Navigator (ICN) and possess strong hands-on experience in EDS and necessary customizations in ICN. I also have a good working knowledge of frameworks like DOJO, and am familiar with build tools such as ANT. I have experience in developing widgets using the DOJO framework, and have a solid understanding of FEM, ACCE, and Case Builder. In addition to my technical skills, I am experienced in training others on FileNet, ICN, ICM and IBM BAW, as well as in working with SOAP web services. I am well-versed in software development processes, having been involved in all phases of the software development lifecycle. With strong interpersonal and communication skills, I have successfully interacted with clients to produce technical solution architectures. I am highly skilled in ECM and have a proven ability to persevere towards challenging goals, leveraging my quick learning ability and self-motivation to drive successful outcomes.`;
 
 export const EXPERIENCES = [
-  
+  {
+    year: "Jan 2024 - May 2025",
+    role: "Sr.Associate -Project(ECM domain)",
+    company: "Cognizant Technologies US Corp - OPTUM/UHG",
+    Client : "OPTUM/UHG",
+    description: [
+      <>
+        Responsibilities, Involved in understanding the requirements.
+      </>,
+      <>
+        Trained and worked in agile methodology.
+      </>,
+      <>
+        Involved during Functional Design understanding exercise.
+      </>,
+      <>
+        Designed and Developed the ICN desktops and involed in customization of ICN Image viewer.Developed the IRIS and PAS image viewer plugins to support the users functionalities in Image viewer.
+      </>,
+      <>
+        Involved in gathering the pre-requisities for migration from Image services to FileNet P8 5.5.
+      </>,
+      <>
+        Designed the Workflows and developed the components.
+      </>,
+      <>
+        Reports generation from database based on search results.
+      </>,
+      <>
+        Involved in application deployment from one environment to another environment
+      </>,
+      <>
+        Involved in the migration of solution from dev,sit,uat
+      </>,
+      <>
+        Support SIT phases and coordinated with QA team on bug fixing
+      </>
+    ],
+    technologies: ["Image Services 4.2 IFix 17, IBM Content Navigator 3.0.11 IFix 17", "Java 1.8", "JDBC", "Log4j", "IBM FileNet Content Engine v5.5", "Dojo 1.10", "IBM Web Sphere Application Server 9.x", "MS SQL Server 2019"],
+  },
   {
     year: "Jan 2024 - May 2025",
     role: "Sr.Associate -Project(ECM domain)",
